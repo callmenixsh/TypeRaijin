dddabf74c3d05538550728b58477eb156e8be9b6
Nixsh <143414492+callmenixsh@users.noreply.github.com>
2024-09-21 04:53:51 +0530

    Hosting3.3

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./helper.css";
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<RouterProvider router={router} />
-	</StrictMode>
+	// <StrictMode>
+		<RouterProvider router={router} />,
+	{/* </StrictMode> */}
 );
